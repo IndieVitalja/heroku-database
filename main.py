2696c83883ad3d7b939f3f75104fb6cfaf4ed4e0
IndieVitalja <vitaljadev@gmail.com>
2021-11-06 20:00:21 +0300

    Add /stats command

--- main.py
+++ main.py
@@ -35,6 +35,22 @@ def start(message):
     update_messages_count(user_id)
 
 
+@bot.message_handler(commands=["stats"])
+def get_stats(message):
+    db_object.execute("SELECT * FROM users ORDER BY messages DESC LIMIT 10")
+    result = db_object.fetchall()
+
+    if not result:
+        bot.reply_to(message, "No data...")
+    else:
+        reply_message = "- Top flooders:\n"
+        for i, item in enumerate(result):
+            reply_message += f"[{i + 1}] {item[1].strip()} ({item[0]}) : {item[2]} messages.\n"
+        bot.reply_to(message, reply_message)
+
+    update_messages_count(message.from_user.id)
+
+
 @bot.message_handler(func=lambda message: True, content_types=["text"])
 def message_from_user(message):
     user_id = message.from_user.id
